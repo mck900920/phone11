565d6f50819c74374ad9708f08c8b35bd67921ce
machenkai <2076756285@qq.com>
2017-08-23 18:10:27 +0800

    111

---
+++ js/home.js
@@ -0,0 +1,24 @@
+// $(function(){
+// 	//header轮播
+
+
+// 	//广告轮播
+// 	 var swiper2 = new Swiper('.advertisement .swiper-container', {
+//         pagination: '.swiper-pagination',
+//         direction: 'vertical',
+//         slidesPerView: 1,
+//         paginationClickable: true,
+//         spaceBetween: 30,
+//         loop : true,
+//         mousewheelControl: true,
+//         autoplay: 2000
+//     });
+
+// 	 //产品列表
+// 	   var swiper3 = new Swiper('.feast .produce-list .swiper-container', {
+//           pagination: '.swiper-pagination',
+//         slidesPerView: 3,
+//         paginationClickable: true,
+//         spaceBetween: 30,
+//     });
+// })
